1bc0ae0e2ddc4673619c87a5b1c4b6ce4fd0d86e
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2025-07-10 22:35:13 +0200

    Implement routing, authentication guards, and toast notifications; add feature components for members, messages, and lists

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -38,6 +38,7 @@ export class AccountService {
   }
 
   logout() {
+    localStorage.removeItem('user');
     this.currentUser.set(null);
   }
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,29 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (response) => {
-        console.log('Login successful', response);
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.toast.success('Login successful');
+        this.creds = {};
       },
-      error: (error) => alert('Login failed: ' + error.message),
+      error: (error) => {
+        this.toast.error(error.error);
+      },
       complete: () => console.log('Login request completed'),
     });
   }
 
   logout() {
     this.accountService.logout();
-    console.log('Logged out successfully');
+    this.router.navigateByUrl('/');
   }
 }
