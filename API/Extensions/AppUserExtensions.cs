80af579d9edf25a799249175c0143d174edad713
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2025-07-27 19:54:31 +0200

    Implement member profile, photos, and messages features; add member resolver and JWT interceptor

--- API/Extensions/AppUserExtensions.cs
+++ API/Extensions/AppUserExtensions.cs
@@ -16,6 +16,7 @@ public static class AppUserExtensions
             Id = user.Id,
             Email = user.Email,
             DisplayName = user.DisplayName,
+            ImageUrl = user.ImageUrl,
             Token = tokenService.CreateToken(user),
         };
     }
